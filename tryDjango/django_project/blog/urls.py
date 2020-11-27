e4f2462c51d5e633c1691c1d47fd7806db1e9173
YatesyTea <danyates1997@gmail.com>
2020-11-27 11:48:47 +0000

    Added a blog section
    Currently following a tutorial to learn the basics of Django.

--- tryDjango/django_project/blog/urls.py
+++ tryDjango/django_project/blog/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
+# Empty path means that it's just in homepage.
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home, name='blog-home'),
+    path('about/', views.about, name='blog-about'),
 ]
